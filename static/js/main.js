ee1d908d734e416952fdeffd3b4854c3678ac6b7
Nacho Navarro <inavarro@paradigmatecnologico.com>
2015-03-04 18:13:12 +0100

    adding smooth navigate between anchors

--- static/js/main.js
+++ static/js/main.js
@@ -32,4 +32,34 @@ $(document).ready(function(){
  $('#main-menu').mCustomScrollbar({theme:'dark-2'});
 });
 
+/*** FIXING SCROLL ***/
+$( document ).ready(function() {
+  var main = main = $('#main-menu');
+
+  $('.toctree-l2 .reference.internal').click(function(event) {
+    event.preventDefault();
+
+    var full_url = this.href,
+      parts = full_url.split('#'),
+      trgt = parts[1],
+      target_offset = $('#'+trgt).offset(),
+      target_top = target_offset.top;
+
+    $('html, body').animate({scrollTop:target_top}, 300);
+
+    /* Remove active class on any li when an anchor is clicked */
+
+    $('#main-menu .active').removeClass('active')
+
+    /* Add active class to clicked anchor's parent li */
+
+    $(this).parent().addClass('active');
+
+  });
+
+});
+
+
+
+
 
